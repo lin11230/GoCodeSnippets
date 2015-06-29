4000e9b861d429d72dc12f53f8d5ecf18f9c5f64
ericlin <ericlin@flysheet.com.tw>
2015-06-29 23:37:26 +0800

    add test code

--- values.go
+++ values.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func main(){
-  
-  fmt.Println("go" + "lang")
+func main() {
 
-  fmt.Println("1+1 = ", 1+1)
+	fmt.Println("go" + "lang")
 
-  fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
+	fmt.Println("1+1 = ", 1+1)
 
-  fmt.Println(true && false)
+	fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
 
-  fmt.Println(true || false)
+	fmt.Println(true && false)
 
-  fmt.Println(!true)
+	fmt.Println(true || false)
+
+	fmt.Println(!true)
 
 }
